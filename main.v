67a9393559bf297b380ee383eb888cf87de95e9b
rooinasuit <roois.code@gmail.com>
2022-12-05 15:28:10 +0100

    minor module instantiation

--- main.v
+++ main.v
@@ -5,21 +5,18 @@ module main (
     output PLACEHOLDER // mayhaps leds
 );
 
-wire SCLK_PULSE;
 wire CTRL_CLK;
+wire SCLK_PULSE;
 
-/*
+wire CS;
+wire SCLK;
+wire MOSI;
+wire MISO;
+
 Gowin_rPLL clk_domains_full(
     .clkout(SCLK_PULSE), // output 20.25 Mhz from divider
     .clkoutd(CTRL_CLK), // output 324 MHz from multiplier (just high enough, could've been 100M)
     .clkin(CLK) // input 27 MHz GLOBAL
-); */
-
-clock_domains clk_domains_lite(
-    .CLK (CLK), 
-    .NRST (NRST),
-    .CTRL_CLK (CTRL_CLK), // 1 MHz
-    .SCLK_PULSE (SCLK_PULSE) // 1kHz
 );
 
 master master(
@@ -35,4 +32,13 @@ master master(
     .MOSI (MOSI)
 );
 
+slave slave1 (
+    .CTRL_CLK (CTRL_CLK),
+    .SCLK_PULSE (SCLK_PULSE),
+    .NRST (NRST),
+    .CS (CS),
+    .SCLK (SCLK),
+    .SDI (MOSI),
+    .SDO (MISO)
+);
 endmodule
